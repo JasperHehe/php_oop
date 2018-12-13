7d3c4ebd1b1669efa446691374dfcf2a4aaf073b
jasper.he <hejpyes@126.com>
2018-12-13 17:49:15 +0800

    1

---
+++ 第四章-高级特性/4.7.php
@@ -0,0 +1,22 @@
+<?php
+final class Checkout{
+
+}
+
+class IllegalCheckout extends Checkout{
+	// 无法继承final类，致命报错
+}
+
+class Check{
+	final function totalize(){
+
+	}
+}
+
+// 可以继承
+class IllegalCheck extends Check{
+	// 无法覆写final方法，致命报错
+	function totalize(){
+		
+	}
+}
