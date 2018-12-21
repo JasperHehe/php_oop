cc489430533a9303a86d9703cce99646aaa9edcd
jasper.he <hejpyes@126.com>
2018-12-21 17:38:42 +0800

    1

---
+++ 第五章-对象和工具/5.3.2.php
@@ -0,0 +1,23 @@
+<?php
+class Test{
+	const SUM_NUM = 100;
+	
+	public static $a = 'lily';
+	protected $b = 'sam';
+	private $c = 'lulu';
+	
+	public function create(){
+		echo self::$a;
+	}
+	
+	protected function set($name){
+		$this->b = $name;
+	}
+
+	private function get(){
+		echo $this->b;
+	}
+}
+
+$m = new ReflectionClass('Test');
+Reflection::export($m);
